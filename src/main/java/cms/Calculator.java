71826bf47a2e830377fe08931848a233cbd242e7
marcantonio99 <marcantonio8799@gmail.com>
2023-06-19 15:46:06 +0200

    calculator and metods

---
+++ src/main/java/cms/Calculator.java
@@ -0,0 +1,24 @@
+package cms;
+
+public class Calculator {
+
+    //creo i metodi della calcolatrice
+    public float add(float num1, float num2) {
+        return num1 + num2;
+    }
+
+    public float subtract(float num1, float num2) {
+        return num1 - num2;
+    }
+
+    public float divide(float num1, float num2) {
+        if (num2 == 0) {
+            throw new ArithmeticException("non si può dividere per zero");
+        }
+        return num1 / num2;
+    }
+
+    public float multiply(float num1, float num2) {
+        return num1 * num2;
+    }
+}
